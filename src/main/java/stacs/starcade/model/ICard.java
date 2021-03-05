d4fd767c68366a465c23bf880a6b039d3c6d2f28
Yoo Chul Kim <yk41@st-andrews.ac.uk>
2021-03-06 01:44:14 +0900

    added enum and changed ints for making it start from 0

--- src/main/java/stacs/starcade/model/ICard.java
+++ src/main/java/stacs/starcade/model/ICard.java
@@ -4,43 +4,47 @@ package stacs.starcade.model;
  * Interface for the representation of a single card.
  */
 public interface ICard {
-//    int number;
-//    int symbol;
-//    int shading;
-//    int colour;
+    enum Colour {BLUE, GREEN, RED}
+    enum Number {ONE, TWO, THREE}
+    enum Shape {TRIANGLE, SQUARE, CIRCLE}
+    enum lineType {DOTTED, DASHED, SOLID}
 
-    int TRIANGLE = 1;
-    int SQUARE = 2;
-    int CIRCLE = 3;
+    int TRIANGLE = 0;
+    int SQUARE = 1;
+    int CIRCLE = 2;
 
-    int BLUE = 1;
-    int GREEN = 2;
-    int RED = 3;
+    int BLUE = 0;
+    int GREEN = 1;
+    int RED = 2;
 
-    int DOTTED = 1;
-    int DASHED = 2;
-    int SOLID = 3;
+    int DOTTED = 0;
+    int DASHED = 1;
+    int SOLID = 2;
 
     /**
      * Getter for the card number of the current card.
+     *
      * @return integer between 1 and 3
      */
     int getNumber();
 
     /**
      * Gets the symbol on the card.
+     *
      * @return integer representing a TRIANGLE, SQUARE or CIRCLE shape
      */
     int getSymbol();
 
     /**
      * Gets the shading of the card.
+     *
      * @return integer representing a DOTTED, DASHED or SOLID shading
      */
     int getShading();
 
     /**
      * Gets the colour of the card.
+     *
      * @return integer representing a BLUE, GREEN or WHITE colour
      */
     int getColour();
