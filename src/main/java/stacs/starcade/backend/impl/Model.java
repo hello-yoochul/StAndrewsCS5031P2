11465c446d5831afd1c6190658e185bc32345174
Matthias <m_erdmann@icloud.com>
2021-03-05 20:44:33 +0000

    Update of ICard, Addition of Card, setCards in Model

--- src/main/java/stacs/starcade/backend/impl/Model.java
+++ src/main/java/stacs/starcade/backend/impl/Model.java
@@ -1,5 +1,46 @@
 package stacs.starcade.backend.impl;
 
+import stacs.starcade.backend.model.Card;
+import stacs.starcade.backend.model.ICard;
+
+import java.util.ArrayList;
+
 public class Model {
+
+    private ICard[] cards;
+
+    public Model() {
+        setCards();
+    }
+
+    /**
+     * Generates an array list of 81 card objects with unique attribute-expression combinations.
+     * @return the array list
+     */
+    private void setCards() {
+        ArrayList<ICard> allCards = new ArrayList<>();
+        int numExpressions = 3;
+
+        for (int i = 0; i < numExpressions; i++) { // Colour
+            for (int j = 0; j < numExpressions; j++) { // Shape
+                for (int k = 0; k < numExpressions; k++) { // Number
+                    for (int l = 0; l < numExpressions; l++) { // Line Style
+
+                        ICard card = new Card();
+                        card.setColour(i);
+                        card.setShape(j);
+                        card.setNumber(k);
+                        card.setLineStyle(l);
+                        allCards.add(card);
+
+                    }
+                }
+            }
+        }
+
+        this.cards = (ICard[]) allCards.toArray();
+    }
+
+    public ICard[] getCards() { return this.cards; }
 
 }
