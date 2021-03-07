bdaf5fe340ab2bed52cb362cb36acc64cc64f34c
Sean <sg280@st-andrews.ac.uk>
2021-03-07 14:48:18 +0000

    Additional twelve cards tests

--- src/test/java/stacs/starcade/backend/impl/ModelTests.java
+++ src/test/java/stacs/starcade/backend/impl/ModelTests.java
@@ -1,12 +1,13 @@
 package stacs.starcade.backend.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import stacs.starcade.shared.Checks;
 import stacs.starcade.shared.ICard;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ModelTests {
 
@@ -25,6 +26,49 @@ public class ModelTests {
     void checkTwelveCards() {
         List<ICard> cards = model.getTwelveCards();
         assertEquals(12, cards.size());
+        int numSets = 0;
+        for (int i = 0; i < cards.size() - 2; i++) {
+            for (int j = i; j < cards.size() - 1; j++){
+                if (j == i){
+                    continue;
+                }
+
+                for (int k = j; k < cards.size(); k++){
+                    if (k == j){
+                        continue;
+                    }
+
+                    if (Checks.isSet(new ICard[]{
+                      cards.get(i),
+                      cards.get(j),
+                      cards.get(k)
+                    })) {
+                        numSets ++;
+                    }
+                }
+            }
+        }
+        assertEquals(5, numSets);
+
+        for (int i = 0; i < cards.size() - 1; i++) {
+            for (int j = i; j < cards.size(); j++) {
+                if (j == i){
+                    continue;
+                }
+                assertNotEquals(cards.get(i), cards.get(j));
+            }
+        }
+    }
+
+    @Test
+    void checkCardsPersist() {
+        List<ICard> cards = model.getTwelveCards();
+        List<ICard> cards2 = model.getTwelveCards();
+        assertEquals(cards.size(), cards2.size());
+
+        for (int i = 0; i < cards.size(); i++) {
+            assertEquals(cards.get(i), cards2.get(i));
+        }
     }
 
 }
