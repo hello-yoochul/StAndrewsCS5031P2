5cb8bff38245044b3d75dd1baa18e2b977b1839d
Yoo Chul Kim <yk41@st-andrews.ac.uk>
2021-03-08 15:59:06 +0900

    made test for cardImageButton

--- src/main/java/stacs/starcade/frontend/view/sub/card/CardImageButton.java
+++ src/main/java/stacs/starcade/frontend/view/sub/card/CardImageButton.java
@@ -1,15 +1,11 @@
 package stacs.starcade.frontend.view.sub.card;
 
-import stacs.starcade.shared.Card;
 import stacs.starcade.shared.ICard;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
-import static stacs.starcade.shared.ICard.*;
-
 
 /**
  * In card view panel, it will have 12 buttons.
@@ -20,23 +16,41 @@ public class CardImageButton extends JButton {
     private ICard card;
     private String imagePathStr;
 
+    /**
+     * Construct CardImageButton with addition of the mouse listener:
+     * if client click the card image button, the button colour will
+     * be changed to show their choice.
+     */
     public CardImageButton() {
         this.addMouseListener(new MyMouseListener());
     }
 
+    /**
+     * Get Image path string of the card.
+     */
     public String getImagePathStr() {
         return imagePathStr;
     }
 
+    /**
+     * Set the card and Store the path of the corresponding card image.
+     */
     public void setCard(ICard card) {
         imagePathStr = card.getColour() + "-" + card.getNumber() + "-" + card.getShape() + "-" + card.getLineStyle() + ".png";
         this.card = card;
     }
 
+    /**
+     * Get the card in the button.
+     */
     public ICard getCard() {
         return this.card;
     }
 
+    /**
+     * Inner class for Mouse Listener. If client click the card image button,
+     * the button colour will be changed to show their chocie.
+     */
     class MyMouseListener extends MouseAdapter {
         public void mouseClicked(MouseEvent e) {
             JButton clickedButton = (JButton) e.getSource();
