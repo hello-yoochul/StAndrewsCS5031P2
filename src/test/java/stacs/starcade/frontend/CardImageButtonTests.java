465d3c2048689c7ce5903a0fb607d5b8f84e3159
Yoo Chul Kim <yk41@st-andrews.ac.uk>
2021-03-08 19:14:06 +0900

    half implemented the card panel to show 12 cards.

--- src/test/java/stacs/starcade/frontend/CardImageButtonTests.java
+++ src/test/java/stacs/starcade/frontend/CardImageButtonTests.java
@@ -2,6 +2,7 @@ package stacs.starcade.frontend;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import stacs.starcade.frontend.model.FrontendModel;
 import stacs.starcade.frontend.view.sub.card.CardImageButton;
 import stacs.starcade.shared.Card;
 import stacs.starcade.shared.ICard;
@@ -14,11 +15,13 @@ import static stacs.starcade.shared.ICard.*;
 import static stacs.starcade.shared.ICard.Number;
 
 public class CardImageButtonTests {
-    CardImageButton cardImageButton;
+    private CardImageButton cardImageButton;
+    private FrontendModel model;
 
     @BeforeEach
     void setup() {
-        cardImageButton = new CardImageButton();
+        model = new FrontendModel();
+        cardImageButton = new CardImageButton(model);
     }
 
     @Test
@@ -28,7 +31,6 @@ public class CardImageButtonTests {
         card.setColour(Colour.RED);
         card.setShape(Shape.TRIANGLE);
         card.setNumber(Number.ONE);
-
         cardImageButton.setCard(card);
 
         assertThat(cardImageButton.getImagePathStr(), is(equalTo("RED-ONE-TRIANGLE-DOTTED.png")));
@@ -41,9 +43,10 @@ public class CardImageButtonTests {
         card.setColour(Colour.RED);
         card.setShape(Shape.TRIANGLE);
         card.setNumber(Number.ONE);
-
         cardImageButton.setCard(card);
 
         assertThat(cardImageButton.getCard(), is(equalTo(card)));
     }
+
+
 }
